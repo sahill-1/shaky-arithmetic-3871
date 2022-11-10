f86364a4320182ef0b3aa5246c31c288399860df
sahill-1 <sv260397@gmail.com>
2022-11-10 22:21:02 +0530

    day 3 homepage

--- bathandbeauty/src/App.js
+++ bathandbeauty/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { AllRoutes } from './Component/AllRoutes/AllRoutes';
 import {Navbar} from "./Component/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <AllRoutes />
     </div>
   );
 }
---
+++ bathandbeauty/src/Component/AllRoutes/AllRoutes.jsx
@@ -0,0 +1,31 @@
+import { Home } from '../Home/Home'
+// import Login from '../Login/Login'
+// import { BodyCare } from '../Products/BodyCare'
+// import { Candles } from '../Products/Candles'
+// import { FallShop } from '../Products/FallShop'
+// import { Gifts } from '../Products/Gifts'
+// import { HandSoap } from '../Products/HandSoap'
+// import { HomeFragrance } from '../Products/HomeFragrance'
+// import { Mens } from '../Products/Mens'
+// import { TopOffer } from '../Products/TopOffer'
+
+export const AllRoutes = () => {
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                {/* <Route path='/top' element={<TopOffer />} />
+                <Route path='/Gifts' element={<Gifts />} />
+                <Route path='/body' element={<BodyCare />} />
+                <Route path='/candles' element={<Candles />} />
+                <Route path='/homefra' element={<HomeFragrance />} />
+                <Route path='/hand' element={<HandSoap />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/fall' element={<FallShop />} />
+                <Route path='/mens' element={<Mens />} />
+                <Route path='/cart' element={<AddCart />} /> */}
+                {/* <Route path="/Des/:id" element={<Des />} /> */}
+            </Routes>
+        </div>
+    )
+}
